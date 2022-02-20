a4d1b9afd682704628339a738eace13603888e2a
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-21 00:08:16 +0300

    + пред тест (не написан негативтест)

--- HW5/src/main/java/Main.java
+++ HW5/src/main/java/Main.java
@@ -1,2 +1,24 @@
+import model.Calculator;
+
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Ввод оператора +,-,*,/");
+        Scanner op = new Scanner(System.in);
+        String line_op = op.nextLine();
+        System.out.println("Ввод числа1");
+        Scanner in1 = new Scanner(System.in);
+        String line_in1 = in1.nextLine();
+        System.out.println("Ввод числа2");
+        Scanner in2 = new Scanner(System.in);
+        String line_in2 = in2.nextLine();
+
+        Calculator.execute(line_op,line_in1,line_in2);
+    }
+
 }
+
+
--- HW5/src/test/java/PositiveCalculatorTest.java
+++ HW5/src/test/java/PositiveCalculatorTest.java
@@ -1,2 +1,26 @@
+import model.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+
+@Test()
 public class PositiveCalculatorTest {
+
+    @DataProvider
+    public Object[][]positiveTest (){
+        return new Object[][]{
+            {"+","4","4","8"},
+            {"-","5","3","2"},
+            {"*","2","3","6"},
+            {"/","8","2","4"}};
+    }
+
+    @Test(dataProvider = "positiveTest")
+    public void positiveTest (String operator,String input1, String input2, String result){
+        Assert.assertEquals(Calculator.execute(operator,input1,input2), result,"Позитивный тест не пройден");
+    }
 }
+
+
