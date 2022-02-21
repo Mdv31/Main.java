a0603964cbd602426bc00f677843fbf635ffe0c0
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-22 00:06:50 +0300

    Add files via upload

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -1,5 +1,6 @@
 import model.Calculator;
-        import org.testng.Assert;
+import model.CalculatorException;
+import org.testng.Assert;
         import org.testng.annotations.DataProvider;
         import org.testng.annotations.Test;
 
@@ -9,18 +10,22 @@ public class NegativeCalculatorTest {
     @DataProvider
     public Object[][]negativeTest (){
         return new Object[][]{
-                {"+","4","-15"},
-                {"-","5","0 3"},
-                {"*","2","a"},
+                {"+","4","a"},
+                {"-","5","-3"},
+                {"*","2","50"},
                 {"/","8","0"}};
     }
 
-    @Test(dataProvider = "negativeTest")
+    @Test(dataProvider = "negativeTest", expectedExceptions  = CalculatorException.class)
     public void negativeTest (String operator,String input1, String input2) {
         String[] params = new String[3];
         params[0]= operator;
         params[1]= input1;
         params[2]= input2;
-        Assert.assertNotNull(Calculator.execute(params),"Значение не 0");
+        //Assert.assertNotNull(Calculator.execute(params),"Значение не 0");
+        //Assert.assertNull(Calculator.execute(params));
+        Calculator.execute(params);
+        Assert.assertFalse(true);
+        //Assert.fail(Calculator.execute(params));
     }
 }
