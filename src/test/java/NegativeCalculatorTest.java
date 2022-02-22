f01ab64899ec662360275e3aed771ab4f0c26bcd
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-22 22:08:21 +0300

    Add files via upload

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -19,12 +19,19 @@ public class NegativeCalculatorTest {
     @Test(dataProvider = "negativeTest")
     public void negativeTest (String operator,String input1, String input2) {
         String[] params = new String[3];
-        params[0]= operator;
-        params[1]= input1;
-        params[2]= input2;
+        params[0] = operator;
+        params[1] = input1;
+        params[2] = input2;
         //Assert.assertNotNull(Calculator.execute(params),"Значение не 0");
         //Assert.assertNull(Calculator.execute(params));
-        Calculator.execute(params);
+        Exception ex = null;
+        try {
+            Calculator.execute(params);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertNull(ex);
+
         //Assert.assertFalse(false);
         //Assert.fail(Calculator.execute(params));
         //Assert.assertTrue(Calculator.execute(params).isEmpty());
