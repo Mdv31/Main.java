a02500fa86161fad529a6c45344fd21408948d19
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-23 09:56:47 +0300

    Add files via upload

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -12,25 +12,27 @@ public class NegativeCalculatorTest {
     public Object[][]negativeData (){
         return new Object[][]{
                 {"+","4","ффф"},
+                {"-","one","two"},
+                {"+","null","null"},
                 {"-","","2"},
                 {"*","2","5 0"},
                 {"/","8","null"}};
     }
-    @Test(dataProvider = "negativeData")//, expectedExceptions = CalculatorException.class)
+    @Test(dataProvider = "negativeData", expectedExceptions = {NumberFormatException.class})
     public void negativeTest (String operator,String input1, String input2){
             String[] params = new String[3];
             params[0] = operator;
             params[1] = input1;
             params[2] = input2;
 
-            boolean flag;
-            try {
+           /* boolean flag;
+            try {*/
                 Calculator.execute(params);
-                flag = false;
+                /*flag = false;
             }catch (Exception e) {
                 flag = true;
             }
 
-        Assert.assertTrue(flag, "исключение получено");
+        Assert.assertTrue(flag, "исключение получено");*/
     }
 }
