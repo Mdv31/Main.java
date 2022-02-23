c035096fcbb3dfe21d5e434a9c78310d5440c178
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-23 09:28:14 +0300

    Add files via upload

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -1,7 +1,9 @@
 import model.Calculator;
 import model.CalculatorException;
-        import org.testng.annotations.DataProvider;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
         import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 
 public class NegativeCalculatorTest {
@@ -10,16 +12,25 @@ public class NegativeCalculatorTest {
     public Object[][]negativeData (){
         return new Object[][]{
                 {"+","4","ффф"},
-                {"-","1","2"},
-                {"*","2","50"},
-                {"/","8","0"}};
+                {"-","","2"},
+                {"*","2","5 0"},
+                {"/","8","null"}};
     }
-    @Test(dataProvider = "negativeData", expectedExceptions = CalculatorException.class)
+    @Test(dataProvider = "negativeData")//, expectedExceptions = CalculatorException.class)
     public void negativeTest (String operator,String input1, String input2){
             String[] params = new String[3];
             params[0] = operator;
             params[1] = input1;
             params[2] = input2;
-            Calculator.execute(params);
+
+            boolean flag;
+            try {
+                Calculator.execute(params);
+                flag = false;
+            }catch (Exception e) {
+                flag = true;
+            }
+
+        Assert.assertTrue(flag, "исключение получено");
     }
 }
